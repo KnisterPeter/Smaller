d4063045586d4802b3e0131f042b1033d6c0653d
Markus Wolf <markus.wolf@sinnerschrader.com>
2012-04-19 18:17:30 +0200

    Started maven client

---
+++ clients/maven/src/main/java/com/sinnerschrader/smaller/clients/maven/SmallerMojo.java
@@ -0,0 +1,20 @@
+package com.sinnerschrader.smaller.clients.maven;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * @author marwol
+ * @goal smaller
+ * @phase process-resources
+ */
+public class SmallerMojo extends AbstractMojo {
+
+  /**
+   * @see org.apache.maven.plugin.Mojo#execute()
+   */
+  public void execute() throws MojoExecutionException, MojoFailureException {
+  }
+
+}
