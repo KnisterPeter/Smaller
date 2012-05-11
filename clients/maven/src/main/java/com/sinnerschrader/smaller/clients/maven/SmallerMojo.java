43de7feb9b9ae120ba00da88a887fd17caeaf479
Markus Wolf <markus.wolf@sinnerschrader.com>
2012-05-11 17:55:38 +0200

    Updated maven client

--- clients/maven/src/main/java/com/sinnerschrader/smaller/clients/maven/SmallerMojo.java
+++ clients/maven/src/main/java/com/sinnerschrader/smaller/clients/maven/SmallerMojo.java
@@ -2,10 +2,15 @@ package com.sinnerschrader.smaller.clients.maven;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
@@ -22,6 +27,20 @@ import com.sinnerschrader.smaller.Zip;
 public class SmallerMojo extends AbstractMojo {
 
   /**
+   * The server host to connect to.
+   * 
+   * @parameter default-value="sr.s2.de"
+   */
+  private String host;
+
+  /**
+   * The server port to connect to.
+   * 
+   * @parameter default-value="1148"
+   */
+  private String port;
+
+  /**
    * A specific <code>fileSet</code> rule to select files and directories.
    * 
    * @parameter
@@ -29,37 +48,80 @@ public class SmallerMojo extends AbstractMojo {
   private FileSet files;
 
   /**
+   * The target folder.
+   * 
+   * @parameter
+   */
+  private File target;
+
+  /**
    * @see org.apache.maven.plugin.Mojo#execute()
    */
   public void execute() throws MojoExecutionException, MojoFailureException {
+    unzip(send(zip()));
+  }
+
+  private OutputStream zip() throws MojoExecutionException {
     try {
-      OutputStream out = createZip();
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+
+      File base = new File(files.getDirectory());
+      FileSetManager fileSetManager = new FileSetManager();
+      files.getDirectory();
+      String[] includedFiles = fileSetManager.getIncludedFiles(files);
+      File temp = File.createTempFile("maven-smaller", ".dir");
+      temp.delete();
+      temp.mkdirs();
+      try {
+        for (String includedFile : includedFiles) {
+          getLog().debug("Adding " + includedFile + " to zip");
+          File target = new File(temp, includedFile);
+          target.getParentFile().mkdirs();
+          FileUtils.copyFile(new File(base, includedFile), target);
+        }
+        Zip.zip(baos, temp);
+      } finally {
+        FileUtils.deleteDirectory(temp);
+      }
+
+      return baos;
     } catch (IOException e) {
-      throw new MojoExecutionException("Failed to create directory structure", e);
+      throw new MojoExecutionException("Failed to create zip file for upload", e);
     }
   }
 
-  private OutputStream createZip() throws IOException {
-    ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-    FileSetManager fileSetManager = new FileSetManager();
-    String[] includedFiles = fileSetManager.getIncludedFiles(files);
-    File temp = File.createTempFile("maven-smaller", ".dir");
-    temp.delete();
-    temp.mkdirs();
+  private InputStream send(OutputStream out) throws MojoExecutionException {
     try {
-      for (String includedFile : includedFiles) {
-        getLog().debug("Adding " + includedFile + " to zip");
-        File target = new File(temp, includedFile);
-        target.getParentFile().mkdirs();
-        FileUtils.copyFile(new File(includedFile), target);
+      CamelContext cc = new DefaultCamelContext();
+      try {
+        cc.start();
+        InputStream in = cc.createProducerTemplate().requestBody("http4://" + host + ':' + port, out, InputStream.class);
+        return in;
+      } finally {
+        cc.stop();
       }
-      Zip.zip(baos, temp);
-    } finally {
-      FileUtils.deleteDirectory(temp);
+    } catch (Exception e) {
+      throw new MojoExecutionException("Failed to send zip file", e);
     }
+  }
 
-    return baos;
+  private void unzip(InputStream in) throws MojoExecutionException {
+    try {
+      File temp = File.createTempFile("smaller", ".zip");
+      temp.delete();
+      FileOutputStream fos = new FileOutputStream(temp);
+      try {
+        IOUtils.copy(in, fos);
+
+        target.mkdirs();
+        Zip.unzip(temp, target);
+      } finally {
+        IOUtils.closeQuietly(fos);
+        temp.delete();
+      }
+    } catch (IOException e) {
+      throw new MojoExecutionException("Failed to handle smaller response", e);
+    }
   }
 
 }
--- clients/maven/src/test/java/com/sinnerschrader/smaller/clients/maven/SmallerMojoTest.java
+++ clients/maven/src/test/java/com/sinnerschrader/smaller/clients/maven/SmallerMojoTest.java
@@ -5,14 +5,27 @@ package com.sinnerschrader.smaller.clients.maven;
 
 import java.io.File;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.*;
 
 /**
  * @author markusw
  */
 public class SmallerMojoTest extends AbstractMojoTestCase {
+
+  private CamelContext camelContext;
 
   /**
    * @see junit.framework.TestCase#setUp()
@@ -20,6 +33,30 @@ public class SmallerMojoTest extends AbstractMojoTestCase {
   @Before
   public void setUp() throws Exception {
     super.setUp();
+    camelContext = new DefaultCamelContext();
+    camelContext.addRoutes(new RouteBuilder() {
+      @Override
+      public void configure() throws Exception {
+        from("jetty:http://localhost:1148/?matchOnUriPrefix=true").process(new Processor() {
+          public void process(Exchange exchange) throws Exception {
+            exchange.getOut().setBody(exchange.getIn().getBody());
+          }
+        });
+      }
+    });
+    camelContext.start();
+  }
+
+  /**
+   * @throws Exception
+   */
+  @After
+  public void teardown() throws Exception {
+    if (camelContext != null) {
+      camelContext.stop();
+      camelContext = null;
+    }
+    super.tearDown();
   }
 
   /**
@@ -27,10 +64,17 @@ public class SmallerMojoTest extends AbstractMojoTestCase {
    */
   @Test
   public void testMojoGoal() throws Exception {
+    File base = new File("target/smaller");
+    FileUtils.deleteDirectory(base);
+
     File testPom = new File(getBasedir(), "src/test/resources/smaller-maven-mojo-config.xml");
     SmallerMojo mojo = (SmallerMojo) lookupMojo("smaller", testPom);
-    assertNotNull(mojo);
+    assertThat(mojo, is(notNullValue()));
     mojo.execute();
+    assertThat(new File(base, "style.less").exists(), is(true));
+    assertThat(new File(base, "a/code.js").exists(), is(true));
+
+    FileUtils.deleteDirectory(base);
   }
 
 }
