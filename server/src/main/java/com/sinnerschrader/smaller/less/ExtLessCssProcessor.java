e84e2c1b07d282e7aa19df019b7ad46afef24985
Markus Wolf <markus.wolf@sinnerschrader.com>
2012-06-21 14:46:55 +0200

    Updated log output

--- server/src/main/java/com/sinnerschrader/smaller/less/ExtLessCssProcessor.java
+++ server/src/main/java/com/sinnerschrader/smaller/less/ExtLessCssProcessor.java
@@ -1,16 +1,11 @@
 package com.sinnerschrader.smaller.less;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-
-import com.sinnerschrader.smaller.common.Manifest;
 
 import ro.isdc.wro.extensions.processor.css.LessCssProcessor;
 import ro.isdc.wro.extensions.processor.support.less.LessCss;
-import ro.isdc.wro.model.resource.Resource;
+
+import com.sinnerschrader.smaller.common.Manifest;
 
 /**
  * @author marwol
@@ -20,13 +15,13 @@ public class ExtLessCssProcessor extends LessCssProcessor {
   private String base;
 
   /**
-   * @param manifest 
+   * @param manifest
    * @param base
    */
   public ExtLessCssProcessor(Manifest manifest, String base) {
     super();
     this.base = base;
-    for (String path: manifest.getCurrent().getIn()) {
+    for (String path : manifest.getCurrent().getIn()) {
       if (path.endsWith(".less")) {
         this.base = new File(this.base, path).getParentFile().getAbsolutePath();
       }
@@ -39,18 +34,6 @@ public class ExtLessCssProcessor extends LessCssProcessor {
   @Override
   protected LessCss newLessCss() {
     return new ExtLessCss(base);
-  }
-
-  /**
-   * @see ro.isdc.wro.extensions.processor.css.LessCssProcessor#process(ro.isdc.wro.model.resource.Resource,
-   *      java.io.Reader, java.io.Writer)
-   */
-  @Override
-  public void process(Resource resource, Reader reader, Writer writer) throws IOException {
-    StringWriter w = new StringWriter();
-    super.process(resource, reader, w);
-    System.err.println(w.toString());
-    writer.write(w.toString());
   }
 
 }
