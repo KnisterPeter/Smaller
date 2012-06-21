64b7515eb8647ec2a1a59d106c9926a580b306cf
Markus Wolf <markus.wolf@sinnerschrader.com>
2012-06-21 10:58:29 +0200

    Added cssembed test

--- server/src/test/java/com/sinnerschrader/smaller/ServerTest.java
+++ server/src/test/java/com/sinnerschrader/smaller/ServerTest.java
@@ -80,7 +80,6 @@ public class ServerTest extends AbstractBaseTest {
     });
   }
 
-  
   /**
    * @throws Exception
    */
@@ -125,6 +124,25 @@ public class ServerTest extends AbstractBaseTest {
             + "rc5a9/Ovbv37+DDi74fT768+fPo06tfz769+/fw48ufT19r9MT3RU9vnJ/6cMj99feZUxIhZR1d1UmnV3IJDoRaTKYR1xuBEKrF14OsNeTZccwh"
             + "WJyH2H3IYIUdhljgfwPu5yBg2eGGYoYM1sacgSYKp6KMLZJII3En3vjiRjPxaGOJK/7YEYciamikfyoeuVqAueX41FAg6pjkczsWieSVUjL5311"
             + "YUshbg2OCGWaXIgV5FJr1tenmm3DGKeecdNZp55145qmnVAEBADs=)}"));
+      }
+    });
+  }
+
+  /**
+   * @throws Exception
+   */
+  @Test
+  public void testCssEmbed() throws Exception {
+    runToolChain("cssembed", new ToolChainCallback() {
+      public void test(File directory) throws Exception {
+        String css = FileUtils.readFileToString(new File(directory, "style-base64.css"));
+        System.out.println(css);
+        assertThat(css, is(".background {\n  background: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAIAAAD/gAIDAA"
+            + "ABaElEQVR42u3aQRKCMAwAQB7n/7+EVx1HbZsEStlcldAsUJrq9hDNsSGABQsWLFiwEMCCBQsWLFgIYMGCBQsWLASwYMGCBQsWAliwYMGCBQtB"
+            + "CGt/j1UrHygTFixYsGDBgnUE1v4lKnK2Zw5h7f0RLGmMLDjCSJlVWOnuYyP8zDwdVkpVKTlnx4o8Dr1YLV8uwUqZ4IP3S1ba1wPnfRsWvZUqVj"
+            + "MnYw1ffFiZBy6OlTvu1bBKZ7rc1f90WJGILx3ujpXSD94Iq/0ssLpPtOYEX7RR03WTro8V2TW7NVbvImOuOWtyr6u2O6fsr8O6LNY8T+JxWEd6"
+            + "/SisGqvlFFvpZsvAenrg0+HBl2DFO97g5S1qthP24NsTVbQ+uQlTurT/WLnNxIS/rQ2UuUVyJXbX6Y16YpvZgXVK41Z3/SLhD7iwYMGCBUvAgg"
+            + "ULFixYAhYsWLBgwRKwYMGCBQuWgAULFixYsAQsWDXxBFVy4xyOC7MdAAAAAElFTkSuQmCC);\n}\n"));
       }
     });
   }
