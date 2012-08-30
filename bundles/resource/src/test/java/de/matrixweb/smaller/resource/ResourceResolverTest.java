5c0086acf776fa44a1c41640c33b45ac8c08c50b
Markus Wolf <markus.wolf@sinnerschrader.com>
2012-08-30 14:11:42 +0200

    Added test for relative file resolving

---
+++ bundles/resource/src/test/java/de/matrixweb/smaller/resource/ResourceResolverTest.java
@@ -0,0 +1,33 @@
+package de.matrixweb.smaller.resource;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+/**
+ * @author marwol
+ */
+public class ResourceResolverTest {
+
+  /**
+   * @throws IOException
+   */
+  @Test
+  public void testRelativeFileResolving() throws IOException {
+    final ResourceResolver resolver = new FileResourceResolver("/tmp");
+    final Resource abc = resolver.resolve("abc.txt");
+    assertThat(abc.getPath(), is("/tmp/abc.txt"));
+    assertThat(abc.getURL().toString(), is("file:/tmp/abc.txt"));
+    final Resource def = abc.getResolver().resolve("subfolder/def.txt");
+    assertThat(def.getPath(), is("/tmp/subfolder/def.txt"));
+    assertThat(def.getURL().toString(), is("file:/tmp/subfolder/def.txt"));
+    final Resource ghi = def.getResolver().resolve("../subfolder2/ghi.txt");
+    assertThat(ghi.getPath(), is("/tmp/subfolder/../subfolder2/ghi.txt"));
+    assertThat(ghi.getURL().toString(),
+        is("file:/tmp/subfolder/../subfolder2/ghi.txt"));
+  }
+
+}
