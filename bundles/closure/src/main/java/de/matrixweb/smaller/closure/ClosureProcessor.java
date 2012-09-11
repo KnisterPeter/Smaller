34513b0adaa769c725d5eb2230568d25f069a075
Markus Wolf <mail@markus-wolf.de>
2012-09-11 08:33:32 +0200

    Updated to latest closure-compiler

--- bundles/closure/src/main/java/de/matrixweb/smaller/closure/ClosureProcessor.java
+++ bundles/closure/src/main/java/de/matrixweb/smaller/closure/ClosureProcessor.java
@@ -1,12 +1,17 @@
 package de.matrixweb.smaller.closure;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.util.Map;
 import java.util.logging.Level;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.javascript.jscomp.ClosureCodingConvention;
 import com.google.javascript.jscomp.CompilationLevel;
@@ -24,6 +29,8 @@ import de.matrixweb.smaller.resource.Type;
  * @author marwol
  */
 public class ClosureProcessor implements Processor {
+
+  private static final LoggerOutputStream LOGGER_OUTPUT_STREAM = new LoggerOutputStream();
 
   /**
    * @see de.matrixweb.smaller.resource.Processor#supportsType(de.matrixweb.smaller.resource.Type)
@@ -49,7 +56,8 @@ public class ClosureProcessor implements Processor {
   private void compile(final Reader reader, final Writer writer)
       throws IOException {
     Compiler.setLoggingLevel(Level.SEVERE);
-    final Compiler compiler = new Compiler();
+    final Compiler compiler = new Compiler(
+        new PrintStream(LOGGER_OUTPUT_STREAM));
     final CompilerOptions compilerOptions = new CompilerOptions();
     compilerOptions.setCodingConvention(new ClosureCodingConvention());
     CompilationLevel.SIMPLE_OPTIMIZATIONS
@@ -61,6 +69,51 @@ public class ClosureProcessor implements Processor {
     if (result.success) {
       writer.write(compiler.toSource());
     }
+  }
+
+  private static class LoggerOutputStream extends OutputStream {
+
+    private static final Logger LOGGER = LoggerFactory
+        .getLogger(LoggerOutputStream.class);
+
+    private final StringBuilder sb = new StringBuilder();
+
+    /**
+     * @see java.io.OutputStream#write(int)
+     */
+    @Override
+    public void write(final int b) throws IOException {
+      this.sb.append((char) b);
+      if ((char) b == '\n') {
+        print();
+      }
+    }
+
+    /**
+     * @see java.io.OutputStream#flush()
+     */
+    @Override
+    public void flush() throws IOException {
+      print();
+      super.flush();
+    }
+
+    /**
+     * @see java.io.OutputStream#close()
+     */
+    @Override
+    public void close() throws IOException {
+      print();
+      super.close();
+    }
+
+    private void print() {
+      if (this.sb.length() > 0) {
+        LOGGER.info(this.sb.toString());
+        this.sb.setLength(0);
+      }
+    }
+
   }
 
 }
