e8b2086b86505aa84a1332a59829f5a93b3bbfc2
Markus Wolf <mail@markus-wolf.de>
2012-10-03 13:23:14 +0200

    Added host and port to start scripts

--- server/osgi-server/src/main/bin/start-local.sh
+++ server/osgi-server/src/main/bin/start-local.sh
@@ -2,6 +2,9 @@
 
 CD=$(dirname $0)
 cd "$CD/.."
-java -jar lib/osgi-kernel-${version}.jar \
+java \
+  -Dorg.apache.felix.host=127.0.0.1 \
+  -Dorg.osgi.service.http.port=1148 \
+  -jar lib/osgi-kernel-${version}.jar \
   -repository=file:${user.home}/.m2/repository/ \
   install:lib/osgi-file-${version}.jar
--- server/osgi-server/src/main/bin/start.sh
+++ server/osgi-server/src/main/bin/start.sh
@@ -2,6 +2,9 @@
 
 CD=$(dirname $0)
 cd "$CD/.."
-java -jar lib/osgi-kernel-${version}.jar \
+java \
+  -Dorg.apache.felix.host=127.0.0.1 \
+  -Dorg.osgi.service.http.port=1148 \
+  -jar lib/osgi-kernel-${version}.jar \
   -repository=http://repo1.maven.org/maven2/ \
   install:lib/osgi-file-${version}.jar
