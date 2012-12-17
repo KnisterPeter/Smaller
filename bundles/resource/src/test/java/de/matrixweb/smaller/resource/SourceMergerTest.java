cecb04082be35aa965261147c1c2958c4c3b110c
Ronny Gärtner <ronny.gaertner@sinnerschrader.com>
2012-12-17 19:35:33 +0100

    add test for multiple source file resolving (default behaviour)

--- bundles/resource/src/test/java/de/matrixweb/smaller/resource/SourceMergerTest.java
+++ bundles/resource/src/test/java/de/matrixweb/smaller/resource/SourceMergerTest.java
@@ -14,9 +14,6 @@ import org.junit.Test;
  */
 public class SourceMergerTest {
 
-  /**
-   * @throws IOException
-   */
   @Test
   public void testUniqueFileResolving() throws IOException {
     final String tempFolder = "/tmp";
@@ -31,5 +28,37 @@ public class SourceMergerTest {
         + "/extensions/js/ext/json2.js"));
     assertThat(resources.get(1).getURL().getPath(), is(tempFolder
         + "/extensions/js/ext/modernizr.custom.js"));
+  }
+
+  @Test
+  public void testMutlipleFileResolving() throws IOException {
+    final String tempFolder = "/tmp";
+    final ResourceResolver resolver = new FileResourceResolver(tempFolder);
+    final SourceMerger merger = new SourceMerger();
+    final List<String> resourcesFiles = new ArrayList<String>();
+    resourcesFiles.add("basic.json");
+    final List<Resource> resources = merger.getResources(resolver,
+        resourcesFiles);
+
+    assertThat(resources.size(), is(9));
+
+    assertThat(resources.get(0).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/json2.js"));
+    assertThat(resources.get(1).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/modernizr.custom.js"));
+    assertThat(resources.get(2).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/modernizr.custom.js"));
+    assertThat(resources.get(3).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/json2.js"));
+    assertThat(resources.get(4).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/modernizr.custom.js"));
+    assertThat(resources.get(5).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/json2.js"));
+    assertThat(resources.get(6).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/json2.js"));
+    assertThat(resources.get(7).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/modernizr.custom.js"));
+    assertThat(resources.get(8).getURL().getPath(), is(tempFolder
+        + "/extensions/js/ext/json2.js"));
   }
 }
