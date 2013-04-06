22c6d0c8220ccde6d5a34c0784e7ea0e1c4068f3
Markus Wolf <mail@markus-wolf.de>
2013-04-06 11:07:11 +0200

    Fixed remote start script

--- server/osgi-server/src/main/bin/start.sh
+++ server/osgi-server/src/main/bin/start.sh
@@ -5,6 +5,6 @@ cd "$CD/.."
 java \
   -Dorg.apache.felix.host=127.0.0.1 \
   -Dorg.osgi.service.http.port=1148 \
-  -jar lib/osgi-kernel-${version}.jar \
+  -jar lib/osgi-kernel-${osgi-kernel-version}.jar \
   -repository=http://repo1.maven.org/maven2/ \
   install:lib/osgi-file-${version}.jar
