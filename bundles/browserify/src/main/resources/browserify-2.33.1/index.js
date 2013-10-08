95ff5413e3e41b7b38996091b8b6e3cc433778b1
Markus Wolf <mail@markus-wolf.de>
2013-10-08 13:19:05 +0200

    Cleanup...

--- bundles/browserify/src/main/resources/browserify-2.33.1/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(command) {
-  return 'bla';
-  var browserify = require('browserify');
-
-  var b = browserify();
-  b.add(command.path + '/main.js');
-  b.bundle().pipe(process.stderr);
-  return command.path + '/main.js';
-}
