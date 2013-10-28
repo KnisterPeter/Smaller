5eb3586e523c728df6db3e5119582267d0d2f0a0
Markus Wolf <markus.wolf@sinnerschrader.com>
2013-10-28 11:37:02 +0100

    Restored Java 6 compatibility

--- old-clients-test/src/test/java/de/matrixweb/smaller/clients/test/Version_0_6_3_Test.java
+++ old-clients-test/src/test/java/de/matrixweb/smaller/clients/test/Version_0_6_3_Test.java
@@ -1,14 +1,12 @@
 package de.matrixweb.smaller.clients.test;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
 
 import de.matrixweb.smaller.common.Manifest;
@@ -32,18 +30,13 @@ public class Version_0_6_3_Test extends AbstractBaseTest {
     System.out.println("Prepare ClassLoader for 0.6.3 client");
     final File clientJar = new File("./target/smaller-client-ant-0.6.3.jar");
     if (!clientJar.exists()) {
-      FileUtils
-          .copyURLToFile(
-              new URL(
-                  "http://search.maven.org/remotecontent?filepath=de/matrixweb/smaller/ant/0.6.3/ant-0.6.3.jar"),
-              clientJar);
+      FileUtils.copyURLToFile(new URL(
+          "http://search.maven.org/remotecontent?filepath=de/matrixweb/smaller/ant/0.6.3/ant-0.6.3.jar"), clientJar);
     }
-    classloader = new URLClassLoader(new URL[] { clientJar.toURI().toURL() },
-        null);
+    classloader = new URLClassLoader(new URL[] { clientJar.toURI().toURL() }, null);
 
     try {
-      clientVersion = Version.getVersion(classloader
-          .loadClass("de.matrixweb.smaller.common.Version")
+      clientVersion = Version.getVersion(classloader.loadClass("de.matrixweb.smaller.common.Version")
           .getMethod("getCurrentVersion").invoke(null).toString());
     } catch (final ClassNotFoundException e) {
       clientVersion = Version.UNDEFINED;
@@ -51,25 +44,16 @@ public class Version_0_6_3_Test extends AbstractBaseTest {
   }
 
   /**
-   * @throws IOException
-   */
-  @AfterClass
-  public static void teardownClassLoader() throws IOException {
-    classloader.close();
-  }
-
-  /**
    * @see de.matrixweb.smaller.AbstractBaseTest#runToolChain(java.lang.String,
    *      de.matrixweb.smaller.AbstractBaseTest.ToolChainCallback)
    */
   @Override
-  protected void runToolChain(final Version minimum, final String file,
-      final ToolChainCallback callback) throws Exception {
+  protected void runToolChain(final Version minimum, final String file, final ToolChainCallback callback)
+      throws Exception {
     if (clientVersion.isAtLeast(minimum)) {
       prepareTestFiles(file, callback, new ExecuteTestCallback() {
         @Override
-        public void execute(final Manifest manifest, final File source,
-            final File target) throws Exception {
+        public void execute(final Manifest manifest, final File source, final File target) throws Exception {
           final de.matrixweb.smaller.common.Task task = manifest.getCurrent();
 
           final Task ant = new Task(classloader);
@@ -88,14 +72,10 @@ public class Version_0_6_3_Test extends AbstractBaseTest {
   }
 
   private Object prepareFileSet(final File source) throws Exception {
-    final Object project = classloader
-        .loadClass("org.apache.tools.ant.Project").newInstance();
-    final Object fileSet = classloader.loadClass(
-        "org.apache.tools.ant.types.FileSet").newInstance();
-    fileSet.getClass().getMethod("setProject", project.getClass())
-        .invoke(fileSet, project);
-    fileSet.getClass().getMethod("setDir", source.getClass())
-        .invoke(fileSet, source);
+    final Object project = classloader.loadClass("org.apache.tools.ant.Project").newInstance();
+    final Object fileSet = classloader.loadClass("org.apache.tools.ant.types.FileSet").newInstance();
+    fileSet.getClass().getMethod("setProject", project.getClass()).invoke(fileSet, project);
+    fileSet.getClass().getMethod("setDir", source.getClass()).invoke(fileSet, source);
     return fileSet;
   }
 
@@ -104,49 +84,40 @@ public class Version_0_6_3_Test extends AbstractBaseTest {
     private final Object task;
 
     Task(final ClassLoader cl) throws Exception {
-      final Class<?> clazz = cl
-          .loadClass("de.matrixweb.smaller.clients.ant.SmallerTask");
+      final Class<?> clazz = cl.loadClass("de.matrixweb.smaller.clients.ant.SmallerTask");
       this.task = clazz.newInstance();
     }
 
     void setProcessor(final String processor) throws Exception {
-      this.task.getClass().getMethod("setProcessor", String.class)
-          .invoke(this.task, processor);
+      this.task.getClass().getMethod("setProcessor", String.class).invoke(this.task, processor);
     }
 
     void setIn(final String in) throws Exception {
-      this.task.getClass().getMethod("setIn", String.class)
-          .invoke(this.task, in);
+      this.task.getClass().getMethod("setIn", String.class).invoke(this.task, in);
     }
 
     void setOut(final String out) throws Exception {
-      this.task.getClass().getMethod("setOut", String.class)
-          .invoke(this.task, out);
+      this.task.getClass().getMethod("setOut", String.class).invoke(this.task, out);
     }
 
     void setOptions(final String options) throws Exception {
-      this.task.getClass().getMethod("setOptions", String.class)
-          .invoke(this.task, options);
+      this.task.getClass().getMethod("setOptions", String.class).invoke(this.task, options);
     }
 
     void setHost(final String host) throws Exception {
-      this.task.getClass().getMethod("setHost", String.class)
-          .invoke(this.task, host);
+      this.task.getClass().getMethod("setHost", String.class).invoke(this.task, host);
     }
 
     void setPort(final String port) throws Exception {
-      this.task.getClass().getMethod("setPort", String.class)
-          .invoke(this.task, port);
+      this.task.getClass().getMethod("setPort", String.class).invoke(this.task, port);
     }
 
     void setFiles(final Object files) throws Exception {
-      this.task.getClass().getMethod("setFiles", files.getClass())
-          .invoke(this.task, files);
+      this.task.getClass().getMethod("setFiles", files.getClass()).invoke(this.task, files);
     }
 
     void setTarget(final File target) throws Exception {
-      this.task.getClass().getMethod("setTarget", File.class)
-          .invoke(this.task, target);
+      this.task.getClass().getMethod("setTarget", File.class).invoke(this.task, target);
     }
 
     void execute() throws Exception {
